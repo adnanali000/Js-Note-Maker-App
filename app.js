5ba4fb833d1232823d6a135637b5062ef4e6ce97
Adnan musil <adnan.museel123@gmail.com>
2020-08-22 17:39:41 +0500

    js note maker app deploy

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ showNotes();
 
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click',function(){
+    //let titleTxt = document.getElementById('titleTxt');
     let addTxt = document.getElementById('addTxt');
+    let addTitle = document.getElementById('addTitle');
+
     let notes = localStorage.getItem('notes');
 
     if(notes == null){
@@ -13,10 +16,16 @@ addBtn.addEventListener('click',function(){
     else{
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem('notes',JSON.stringify(notesObj));
     addTxt.value = "";
-   showNotes();
+    addTitle.value = "";  
+     showNotes();
 
 })
 
@@ -24,6 +33,7 @@ addBtn.addEventListener('click',function(){
 //show notes
 
 function showNotes(){
+    //let titleTxt = document.getElementById('titleTxt');
     let notes = localStorage.getItem('notes');
     if(notes == null){
         notesObj = [];
@@ -37,8 +47,8 @@ function showNotes(){
         html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 
         <div class="card-body">
-          <h5 class="card-title">Note ${index + 1}</h5>
-          <p class="card-text">${element}</p>
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text">${element.text}</p>
           <button id = ${index} onclick = "deleteNote(this.id)"  class="btn btn-primary">Delete Note</button>
         </div>
       </div>`
@@ -83,8 +93,8 @@ search.addEventListener('input',function(){
 
     Array.from(noteCard).forEach(function(element){
        let cardTxt = element.getElementsByTagName('p')[0].innerText;
-
-       if(cardTxt.includes(searchVal)){
+       let textTitle = element.getElementsByTagName('h5')[0].innerHTML; 
+       if(cardTxt.includes(searchVal) || textTitle.includes(searchVal)){
            element.style.display = "block";
        }
        else{
